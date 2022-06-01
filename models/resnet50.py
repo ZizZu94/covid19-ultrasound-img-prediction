00514073e27351723cdd4649f0755ad3214d0934
ZizZu94 <azamzihadul@hotmail.it>
2022-06-01 12:44:18 +0200

    Fix ResNet

--- models/resnet50.py
+++ models/resnet50.py
@@ -4,32 +4,30 @@ import torchvision
 from torchvision import models
 
 class CustomResNet50(nn.Module):
-    def __init__(self, img_w, img_h, class_num, dropout = 0.4):
+    def __init__(self, class_num, dropout = 0.4):
         super(CustomResNet50, self).__init__()
 
-        self.img_w = img_w
-        self.img_h = img_h
         self.class_num = class_num
         self.dropout = dropout
 
         backbone = models.resnet50(pretrained=True)
         self.fc_inputs = backbone.fc.in_features
 
-        base_layers = list(backbone.children())[:-1]
+        self.feature_extractor = nn.Sequential(*list(backbone.children())[:-2]) # we take layers before the classifier and the avgpool
 
-        self.feature_extractor = nn.Sequential(*base_layers)
+        self.avgpool = backbone.avgpool
 
         # classifier
         self.out = nn.Sequential(
-            nn.Linear(self.fc_inputs, 256, bias=True),
+            nn.Linear(self.fc_inputs, 1024, bias=True),
             nn.ReLU(),
             nn.Dropout(self.dropout),
-            nn.Linear(256, self.class_num),
-            #nn.LogSoftmax(dim=1) # For using NLLLoss()
+            nn.Linear(1024, self.class_num),
         )
     
     def forward(self, x):
         x = self.feature_extractor(x)
+        x = self.avgpool(x)
         x = x.view(x.shape[0], -1)  # reshape the tensor
         x = self.out(x)
         return x
